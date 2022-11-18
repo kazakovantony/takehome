9ffa275a8972b74e7009da1330b8b7083938b5ee
akazakov <kazakov.antony@gmail.com>
2022-11-18 09:31:34 +0300

    Automation test remember session id

--- src/test/java/com/example/takehome/TakehomeApplicationIntegrationTest.java
+++ src/test/java/com/example/takehome/TakehomeApplicationIntegrationTest.java
@@ -6,6 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,8 +23,17 @@ public class TakehomeApplicationIntegrationTest {
     @Test
     public void Given_Countries_When_ValidRequest_Then_ValidResponse() {
 
-        var response = testRestTemplate.getForEntity("/neighbors?countries=US,CA",
-                                                     ContinentResponse.class).getBody();
+        List<String> coockies = testRestTemplate.getForEntity("/neighbors?countries=US,CA",
+                                                              ContinentResponse.class).getHeaders().get("Set-Cookie");
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.put(HttpHeaders.COOKIE, coockies);
+        HttpEntity<Void> requestEntity = new HttpEntity<>(requestHeaders);
+
+        var response = testRestTemplate.exchange("/neighbors?countries=US,CA",
+                                                 HttpMethod.GET,
+                                                 requestEntity,
+                                                 ContinentResponse.class).getBody();
+
         assertThat(response.getContinent().get(0).getName()).isEqualTo("North America");
     }
 }
